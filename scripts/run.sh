c4556071c9e5c6b6c070b2997c5c1656764545d5
Benedetta Tremolada <97625253+benedettatremolada@users.noreply.github.com>
2024-10-25 15:10:42 +0200

    Create run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+JSON_FILE=/afs/cern.ch/work/d/dapullia/public/dune/protodune/event_plotter/json/29425.json
+
+cd /afs/cern.ch/work/d/dapullia/public/dune/protodune/event_plotter/app
+python plotter.py --input_json $JSON_FILE 
+cd /afs/cern.ch/work/d/dapullia/public/dune/protodune/event_plotter/scripts
